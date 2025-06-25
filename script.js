be121fe3494085113cea57d8c6dc8e90f65a2f9c
MarcelBossASG <marcel.thebosss@gmail.com>
2025-06-25 19:07:17 +0100

    Website Overhaul
    many changes

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector(".menu-icon");
-    const dropdown = document.querySelector(".dropdown");
-
-    function toggleDropdown() {
-        if (dropdown.classList.contains("show")) {
-            dropdown.classList.remove("show");
-            dropdown.style.display = "none";
-        } else {
-            dropdown.classList.add("show");
-            dropdown.style.display = "block";
-        }
-    }
-
-    function toggleMenuIcon() {
-        if (menuIcon.classList.contains("active")) {
-            menuIcon.classList.remove("active"); 
-        } else {
-            menuIcon.classList.add("active");
-        }
-    }
-
-    menuIcon.addEventListener("click", function() {
-        toggleDropdown();
-        toggleMenuIcon();
-    });
-
-    document.addEventListener("click", function(event) {
-        if (!menuIcon.contains(event.target) && !dropdown.contains(event.target)) {
-            dropdown.classList.remove("show");
-            dropdown.style.display = "none";
-            menuIcon.classList.remove("active");
-        }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.fade-in').classList.add('visible');
 });
