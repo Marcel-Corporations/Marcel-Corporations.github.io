bc146463459bc93f58372b2b59b11ba9619bee9e
MarcelBossASG <marcel.thebosss@gmail.com>
2025-06-25 20:21:13 +0100

    Mobile Support
    added mobile support

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.fade-in').classList.add('visible');
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+  if (mobileMenu.style.display === 'flex') {
+    mobileMenu.style.display = 'none';
+  } else {
+    mobileMenu.style.display = 'flex';
+  }
+});
